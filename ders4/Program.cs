ca9cfbdb6881e921ca4168e7f05846372cdecc53
samandargaibov <samandar.gaibov@gmail.com>
2022-12-22 21:42:27 +0400

    add for

--- ders4/Program.cs
+++ ders4/Program.cs
@@ -6,7 +6,18 @@ namespace ders4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!");
+           // Console.WriteLine("Hello World!!!!");
+
+            //1-den n-else kimi 7ye bolunen ededler
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 7; i < n; i+=10)
+            {
+               
+               Console.WriteLine(i);
+
+            }
         }
     }
 }
